3f949685294ec0ca3a861ed174ff8bb6d4690e33
Jose Carlos <josealarconchacon@gmail.com>
2020-12-11 14:14:12 -0500

    first commit

---
+++ Mad Libs/Mad Libs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mad Libs
+//
+//  Created by Jose Alarcon Chacon on 12/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
