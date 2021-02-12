ab94a9364254017f3c080f7a3a0cc42569542129
Jose Carlos <josealarconchacon@gmail.com>
2021-02-12 11:03:36 -0500

    Creating CLLocationManager

--- LocationManager/LocationManager/ViewController.swift
+++ LocationManager/LocationManager/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationM: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager()
     }
-
-
+    
+    func locationManager() {
+        locationM = CLLocationManager()
+        locationM.delegate = self
+        locationM.desiredAccuracy = kCLLocationAccuracyBest
+        locationM.distanceFilter = 0.0
+        
+        // Request permition for user's location
+        locationM.requestWhenInUseAuthorization()
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationM.startUpdatingLocation()
+        }
+    }
 }
 
