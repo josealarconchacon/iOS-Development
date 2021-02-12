4e49bc26955cc2818a00e47b1d85854d1456682f
Jose Carlos <josealarconchacon@gmail.com>
2021-02-12 11:59:00 -0500

    user location practice

--- MapView/MapView/ViewController.swift
+++ MapView/MapView/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 0.0
+        
+        // Request permission for user location
+        locationManager.requestWhenInUseAuthorization()
+        
+        if locationManager.authorizationStatus == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
     }
-
-
+    // Delegate method
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        print("location update latitude: \(location.coordinate.latitude)  longitude: \(location.coordinate.longitude)")
+    }
 }
 
